6c783534e227eb80270a3074119125b55618b4a7
Ashwani Kumar Bachchan <akb762@gmail.com>
2020-03-03 22:07:32 +0530

    Initial Commit
    Signed-off-by: Ashwani Kumar Bachchan <akb762@gmail.com>

---
+++ app/src/main/cpp/helloworld-c.cpp
@@ -0,0 +1,25 @@
+//
+// Created by Ashwani on 29-02-2020.
+//
+
+#include <android_native_app_glue.h>
+#include <jni.h>
+
+extern "C" {
+void handle_cmd(android_app *pApp, int32_t cmd) {
+}
+
+void android_main(struct android_app *pApp) {
+    pApp->onAppCmd = handle_cmd;
+
+    int events;
+    android_poll_source *pSource;
+    do {
+        if (ALooper_pollAll(0, nullptr, &events, (void **) &pSource) >= 0) {
+            if (pSource) {
+                pSource->process(pApp, pSource);
+            }
+        }
+    } while (!pApp->destroyRequested);
+}
+}
